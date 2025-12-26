f56185646c82f395295aeaf0fab59c357f31bca5
shaxenn <bydkhakan07@gmail.com>
2025-12-26 23:45:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,42 +1,39 @@
 import discord
 from discord.ext import commands
-import requests
 import random
-
-# ===== AYARLAR =====
-DISCORD_TOKEN = "BOT_TOKEN"
-GIPHY_API_KEY = "GIPHY_API_KEY"
-PREFIX = "!w"
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+bot = commands.Bot(command_prefix="!w ", intents=intents)
+
+GIPHY_API_KEY = ""
+
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} Active!")
+    print(f"Aktif: {bot.user}")
+
 
 @bot.command()
-async def gif(ctx, *, gif_turu: str):
+async def gif(ctx, *, tur: str):
     url = "https://api.giphy.com/v1/gifs/search"
-    params = {
-        "api_key": GIPHY_API_KEY,
-        "q": gif_turu,
-        "limit": 25,
-        "rating": "pg"
-    }
+
+    params = {"api_key": GIPHY_API_KEY, "q": tur, "limit": 25, "rating": "pg"}
 
     response = requests.get(url, params=params)
     data = response.json()
 
     if not data["data"]:
-        await ctx.send("❌ Bu türde gif bulamadım.")
+        await ctx.send("Bu türde GIF bulamadım 😢")
         return
 
-    secilen_gif = random.choice(data["data"])
-    gif_url = secilen_gif["images"]["original"]["url"]
+    gif = random.choice(data["data"])
+    gif_url = gif["images"]["original"]["url"]
 
     await ctx.send(gif_url)
 
-bot.run(DISCORD_TOKEN)
+
+bot.run(
+    "DISCORD_TOKEN")
